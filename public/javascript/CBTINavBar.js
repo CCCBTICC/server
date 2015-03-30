36334d127b962e5e252a1bf3efb7464b122344c6
ypling <leoanrd.li.pub@gmail.com>
2015-03-29 20:08:38 -0700

    1 changed the name of bootstrap collapse to navbar-collaspase in CBTINavBarTemplate.html; 2 add function to hide collapse when click menu button in CBTINavBar.js

--- public/javascript/CBTINavBar.js
+++ public/javascript/CBTINavBar.js
@@ -1,8 +1,12 @@
-
-
-angular.module('CBTINavBar',[]).directive('navBar',function(){
-    return{
-        restrict:'EA',
-        templateUrl:'javascript/CBTINavBarTemplate.html'
+angular.module('CBTINavBar', []).directive('navBar', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: 'javascript/CBTINavBarTemplate.html',
+        link: function (scope, element, attrs) {
+            scope.menuOnClick = function (id) {
+                $('#' + id).tab('show');
+                $("#navbar-collapse").collapse('hide');
+            }
+        }
     }
 });
