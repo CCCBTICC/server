820ea114d74b6f6bd6647b4d2ae7e71be3225202
lhz213 <lhz_work@hotmail.com>
2015-03-30 01:06:26 -0700

    fix bug

--- public/javascript/CBTINavBar.js
+++ public/javascript/CBTINavBar.js
@@ -3,6 +3,12 @@
 angular.module('CBTINavBar',[]).directive('navBar',function(){
     return{
         restrict:'EA',
-        templateUrl:'javascript/CBTINavBarTemplate.html'
+        templateUrl:'javascript/CBTINavBarTemplate.html',
+        link: function (scope, element, attrs) {
+            scope.menuOnClick = function (id) {
+                $('#' + id).tab('show');
+                $("#navbar-collapse").collapse('hide');
+            }
+        }
     }
 });
